0c38c0d52b465e1dec175128e598a9141b59f9ef
Jose Manuel Oviedo <thejm1133@gmail.com>
2025-09-18 15:55:06 -0400

    Add Loading component with loader icon and loading message

---
+++ src/components/Loading.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Loader from "@/components/icons/Loader.vue";
+</script>
+
+<template>
+  <p class="inline-flex items-center gap-2 text-lg font-medium text-white/60">
+    <Loader /> Loading, please wait...
+  </p>
+</template>
